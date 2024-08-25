f380a21a0f6f092f467303c93b0dcbe0c9dcf5da
Philip Levy <pglevy@users.noreply.github.com>
2024-08-25 13:34:40 -0400

    Initial commit

---
+++ apps/home/views.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django import template
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from .models import Contact
+from django.contrib import messages
+
+
+@login_required(login_url="/login/")
+def index(request):
+    context = {'segment': 'index'}
+
+    html_template = loader.get_template('home/index.html')
+    return HttpResponse(html_template.render(context, request))
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('name')
+        message = request.POST.get('name')
+
+        new_contact = Contact(name=name,email=email,message=message)
+        new_contact.save()
+        messages.success(request,f"Message sent {name.split(' ')[0]} !")
+
+    context = {'segment': 'contact-us.html'}
+
+    html_template = loader.get_template('home/contact-us.html')
+    return HttpResponse(html_template.render(context, request))
+
+
+@login_required(login_url="/login/")
+def pages(request):
+    context = {}
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+    try:
+
+        load_template = request.path.split('/')[-1]
+
+        if load_template == 'admin':
+            return HttpResponseRedirect(reverse('admin:index'))
+        context['segment'] = load_template
+
+        html_template = loader.get_template('home/' + load_template)
+        return HttpResponse(html_template.render(context, request))
+
+    except template.TemplateDoesNotExist:
+
+        html_template = loader.get_template('home/page-404.html')
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+        html_template = loader.get_template('home/page-500.html')
+        return HttpResponse(html_template.render(context, request))
